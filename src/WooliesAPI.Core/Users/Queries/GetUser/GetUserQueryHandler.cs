e8f92dc20fcd98716b783a3e2c456df62476c92b
Nicholas Fane <nicholas.fanedev@gmail.com>
2019-07-08 12:37:36 +1000

    Added basic structure of project, CQS implementation of User endpoint, mocked user data for the time being.

---
+++ src/WooliesAPI.Core/Users/Queries/GetUser/GetUserQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WooliesAPI.Domain.Entities;
+
+namespace WooliesAPI.Core.Users.Queries.GetUser
+{
+    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, User>
+    {
+        public async Task<User> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            // TODO
+            // Implement DB context and retrieve from there
+            return new User()
+            {
+                Name = "test-user",
+                Token = "6e424f40-80a9-49b8-8d66-5921a6734555"
+            };
+        }
+    }
+}
---
+++ src/WooliesAPI.Web/Controllers/AnswersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WooliesAPI.Core.Users.Queries.GetUser;
+using WooliesAPI.Domain.Entities;
+
+namespace WooliesAPI.Web.Controllers
+{
+    [ApiController]
+    public class AnswersController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<User>> User()
+        {
+            var result = await Mediator.Send(new GetUserQuery());
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound();
+        }
+    }
+}
